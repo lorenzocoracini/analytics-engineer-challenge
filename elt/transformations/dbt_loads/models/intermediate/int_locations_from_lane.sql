836b3c093989736fb32f6b2c550fa6f44a848c6d
Lorenzo <lorenzo291909@gmail.com>
2026-09-09 13:59:35 -0300

    feat: split lane logic, intermediate model creation.

---
+++ elt/transformations/dbt_loads/models/intermediate/int_locations_from_lane.sql
@@ -0,0 +1,24 @@
+{{ config(
+    materialized='view',
+    tags=['intermediate']
+) }}
+
+with source as (
+  select 
+    loadsmart_id,
+    lane
+  from {{ ref('stg_loads') }}
+),
+
+parsed_lanes as (
+  select 
+    loadsmart_id,
+    lane,    
+    trim(split_part(split_part(lane, '->', 1), ',', 1)) as pickup_city,
+    trim(split_part(split_part(lane, '->', 1), ',', 2)) as pickup_state,    
+    trim(split_part(split_part(lane, '->', 2), ',', 1)) as delivery_city,
+    trim(split_part(split_part(lane, '->', 2), ',', 2)) as delivery_state
+  from source
+)
+
+select * from parsed_lanes
