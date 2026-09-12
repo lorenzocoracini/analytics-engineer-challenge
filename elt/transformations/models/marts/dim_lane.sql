ec1a0da6d86ec6c13e0be4bb6edc405d7cf03515
Lorenzo <lorenzo291909@gmail.com>
2026-09-11 22:06:23 -0300

    fix: remove mileage from dim.

--- elt/transformations/models/marts/dim_lane.sql
+++ elt/transformations/models/marts/dim_lane.sql
@@ -15,14 +15,12 @@ lanes as (
     {{ dbt_utils.generate_surrogate_key(['pickup_city', 'pickup_state']) }} as source_location_id,    
     {{ dbt_utils.generate_surrogate_key(['delivery_city', 'delivery_state']) }} as target_location_id,    
     trim(pickup_city) || ',' || trim(pickup_state) || ' -> ' || trim(delivery_city) || ',' || trim(delivery_state) as lane_name,    
-    mileage,    
     current_timestamp as dbt_created_at 
   from source
   where pickup_city is not null 
     and pickup_state is not null 
     and delivery_city is not null 
     and delivery_state is not null
-    and mileage is not null
 )
 
 select * from lanes
